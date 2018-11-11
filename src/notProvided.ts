e6a99097ac6ef8ca0e3bb5c47e373021bfb298f9
Gordon Leigh <gordon.leigh@futurematik.co.uk>
2018-11-11 17:57:47 +0000

    add allowNull option to notProvided

--- src/notProvided.ts
+++ src/notProvided.ts
@@ -5,9 +5,14 @@ export const UnexpectedField = 'UNEXPECTED_FIELD';
 /**
  * Require value not to be present. Best used with or().
  */
-export function notProvided(): ValueValidator<undefined> {
+export function notProvided(opts?: {
+  allowNull?: boolean;
+}): ValueValidator<undefined> {
   return ctx => {
-    if (ctx.value === undefined) {
+    if (
+      ctx.value === undefined ||
+      (opts && opts.allowNull && ctx.value === null)
+    ) {
       return [];
     }
     return [
