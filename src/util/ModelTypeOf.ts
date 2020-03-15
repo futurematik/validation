029d707f1dba1fa2b89f61b25f20d4b5464466f7
Gordon Leigh <gordon.leigh@futurematik.co.uk>
2020-03-15 10:24:44 +0000

    expand ModelTypeOf to include ValueValidators

--- src/util/ModelTypeOf.ts
+++ src/util/ModelTypeOf.ts
@@ -1,6 +1,10 @@
-import { ModelDefinition } from '../core';
+import { ModelDefinition, ValueValidator } from '../core';
 
 /**
  * Return the model type.
  */
-export type ModelTypeOf<T> = T extends ModelDefinition<infer M> ? M : never;
+export type ModelTypeOf<T> = T extends ValueValidator<infer M>
+  ? M
+  : T extends ModelDefinition<infer M>
+  ? M
+  : never;
